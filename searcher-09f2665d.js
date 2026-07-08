ebc5df190196a4e0daf83c586bd7e785bc72f2bc
hailiu88 <3348194517@qq.com>
2026-07-08 14:41:21 -0400

    fix: change online reading link from http to https

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-9c13310b.js',
+                path_to_root + 'searchindex-98a75085.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
